0cd94830fbaeb6f9a04905118b16ab657b7b1fd8
viktor_tumanov <viktor.tumanov@waveaccess.ru>
2023-10-05 14:52:16 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import logger from 'morgan'
+import * as path from 'path'
+
+//routes
+import streamRouter from './modules/stream/stream.controller'
+import contentRouter from './modules/content/content.controller'
+
+// middleware
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+
+
+//endpoints
+app.use('/stream', streamRouter)
+app.use('/content', contentRouter)
+
+const PORT = process.env.Port || 8080
+
+app.listen(PORT, () => {
+  console.log("Поехале!!!")
+  console.log(`http://localhost:${PORT}`)
+})
+
+
+
---
+++ src/modules/stream/stream.controller.ts
@@ -0,0 +1,53 @@
+import {Router} from 'express'
+import WebTorrent from 'webtorrent'
+
+const router = Router()
+const client = new WebTorrent()
+
+let state = {
+  progress: 0,
+  downloadspeed: 0,
+  ratio: 0
+}
+
+let error;
+
+client.on('error', (err: Error) => {
+  console.error('err', err.message)
+  error = err.message
+})
+
+client.on('torrent', () => {
+  console.log(client.progress)
+  state = {
+    progress: Math.round(client.progress * 100 * 100) / 100,
+    downloadspeed: client.downloadspeed,
+    ratio: client.ratio
+  }
+})
+
+router.get('/add/:magnet', (req, res) => {
+const magnet = req.params.magnet
+
+client.add(magnet, torrent => {
+    const files = torrent.files.map(data => ({
+      name: data.name, 
+      length: data.length,
+    }))
+
+    res.status(200).send(files)
+  })
+
+})
+
+router.get('/stats', (req, res) => {
+  state = {
+    progress: Math.round(client.progress * 100 * 100) / 100,
+    downloadspeed: client.downloadspeed,
+    ratio: client.ratio
+  }
+  res.status(200).send(state)
+})
+
+export default router
+
